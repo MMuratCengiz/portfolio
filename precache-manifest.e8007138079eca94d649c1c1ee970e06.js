382a06b6b2081fd8b3cd4a2ee199aaf0cdaaf89e
Murat Cengiz <cengizm9@gmail.com>
2020-03-08 00:01:13 +0100

    Updates

---
+++ precache-manifest.e8007138079eca94d649c1c1ee970e06.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16fe267fc6e7a5c4c4bd050c3ec0d52b",
+    "url": "/porfolio/index.html"
+  },
+  {
+    "revision": "0054d6ee766681fc362a",
+    "url": "/porfolio/static/css/main.f2163d53.chunk.css"
+  },
+  {
+    "revision": "02622e9d70db10e7705c",
+    "url": "/porfolio/static/js/2.214daba2.chunk.js"
+  },
+  {
+    "revision": "ad0057db05a0df56f2cb93b8ebfd013c",
+    "url": "/porfolio/static/js/2.214daba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0054d6ee766681fc362a",
+    "url": "/porfolio/static/js/main.7c16a658.chunk.js"
+  },
+  {
+    "revision": "f1234b88aba7e0df425b",
+    "url": "/porfolio/static/js/runtime-main.540e5bdf.js"
+  },
+  {
+    "revision": "918a4e6a662794771e3e0187969bd0c1",
+    "url": "/porfolio/static/media/bg4.918a4e6a.png"
+  },
+  {
+    "revision": "52aa64c676b7eae8bde49612cb7244f8",
+    "url": "/porfolio/static/media/ochess.52aa64c6.gif"
+  },
+  {
+    "revision": "2829900591bca13c8007b177ec1ea173",
+    "url": "/porfolio/static/media/profile.28299005.png"
+  }
+]);
